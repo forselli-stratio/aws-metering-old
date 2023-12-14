5051ddedb160360805c7c48caf7ad8916eed0db2
Francisco Orselli <forselli@stratio.com>
2023-12-14 10:59:25 +0100

    Working

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # aws-metering
+
+https://awsmp-loadforms.s3.amazonaws.com/AWS+Marketplace+-+SaaS+Integration+Guide.pdf
+
+```
+spec:
+  productCode: "Stratio"
+  customerIdentifier: "Customer"
+  dimenssions:
+    - name: cpu
+      prometheusQuery: "billing:cpu_usage:last1h"
+    - name: mem
+      prometheusQuery: "billing:mem_usage:last1h"
+    - name: storage
+      prometheusQuery: "billing:storage_usage:last1h"
+
+```
+
+Funcionamiento:
+
+1. Aplicación arranca y envía registros del momento actual obteniendo los datos con query a prometheus
+last_prometheus_query_success
+last_report_success
+
+2. Comprobamos en la respuesta si existen UnprocessedRecords
+unprocessed_records
+
+3. Si existen unprocessedrecords se reintenta su envío.
+unprocessed_records_retried
+
+4. Comprobamos en la respuesta siguen existiendo UnprocessedRecords y se actualiza metrica tras reintentos.
+unprocessed_records
+
+4. Se ejecuta el mismo ciclo cada x minutos
--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
-	"os"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -15,68 +15,116 @@ import (
 )
 
 const (
-    prometheusURL       = "http://localhost:9090"
-	productCode     = "STRATIO"
+	prometheusURL      = "http://localhost:9090"
+	productCode        = "STRATIO"
 	customerIdentifier = "CUSTOMER"
-	dimensionName    = "CPU"
+	cpuDimension       = "CPU"
+	memDimension       = "MEM"
+	storageDimension   = "STORAGE"
+	cpuQuery           = "billing:cpu_usage:last1h"
+	memQuery           = "billing:mem_usage:last1h"
+	storageQuery       = "billing:storage_usage:last1h"
 )
 
 func main() {
+	interval := flag.Duration("interval", time.Hour, "Execution interval duration (e.g., 1h)")
+	flag.Parse()
+	// Run the initial execution
+	run()
 
-    // Initialize Prometheus client
-    promClient, err := api.NewClient(api.Config{
-        Address: prometheusURL,
-    })
-    if err != nil {
-        log.Fatal("Error creating Prometheus client: ", err)
+	// Schedule execution at the specified interval
+	ticker := time.NewTicker(*interval)
+
+    for range ticker.C {
+        run()
     }
 
-    // Create Prometheus API client
-    promAPI := v1.NewAPI(promClient)
-
-    // Get CPU capacity metric from Prometheus
-    cpuCapacity, err := getCPUCapacity(promAPI)
-    if err != nil {
-        log.Fatal("Error getting CPU capacity: ", err)
-    }
-
-	metricValue := int64(cpuCapacity[0].Value)
-	metricTimestamp := cpuCapacity[0].Timestamp.Time()
-
-
-    sendMeteringRecords(metricValue, metricTimestamp)
 }
 
-func getCPUCapacity(promAPI v1.API) (model.Vector, error) {
-    // Query Prometheus for cpu_usage in the last hour
-    query := `billing:cpu_usage:last1h`
-    result, warnings, err := promAPI.Query(context.Background(), query, time.Now())
-    if err != nil {
-        fmt.Printf("Error querying Prometheus: %v\n", err)
-        os.Exit(1)
-    }
-    if len(warnings) > 0 {
-        fmt.Printf("Warnings: %v\n", warnings)
-    }
+func run() {
+	promAPI, err := initPrometheusAPI(prometheusURL)
+	if err != nil {
+		log.Printf("Error creating Prometheus client: %v", err)
+		return
+	}
 
-	cpuCapacity := result.(model.Vector)
-    return cpuCapacity, nil
+	cpuMetricValue, cpuMetricTimestamp, err := getMetric(promAPI, cpuQuery)
+	if err != nil {
+		log.Printf("Error getting CPU capacity: %v", err)
+		return
+	}
+
+	memMetricValue, memMetricTimestamp, err := getMetric(promAPI, memQuery)
+	if err != nil {
+		log.Printf("Error getting MEM capacity: %v", err)
+		return
+	}
+
+	storageMetricValue, storageMetricTimestamp, err := getMetric(promAPI, storageQuery)
+	if err != nil {
+		log.Printf("Error getting STORAGE capacity: %v", err)
+		return
+	}
+
+	fmt.Println(createMeteringRecords(cpuMetricValue, memMetricValue, storageMetricValue, cpuMetricTimestamp, memMetricTimestamp, storageMetricTimestamp))
 }
 
-func sendMeteringRecords(value int64, timestamp time.Time) {
-    timezone, _ := time.LoadLocation("UTC")
+func initPrometheusAPI(prometheusURL string) (v1.API, error) {
+	promClient, err := api.NewClient(api.Config{
+		Address: prometheusURL,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return v1.NewAPI(promClient), nil
+}
+
+func getMetric(promAPI v1.API, query string) (int64, time.Time, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	result, warnings, err := promAPI.Query(ctx, query, time.Now(), v1.WithTimeout(5*time.Second))
+	if err != nil {
+		return 0, time.Time{}, fmt.Errorf("error querying prometheus: %v", err)
+	}
+	if len(warnings) > 0 {
+		fmt.Printf("Warnings: %v\n", warnings)
+	}
+
+    vector, ok := result.(model.Vector)
+    if !ok || len(vector) == 0 {
+        return 0, time.Time{}, fmt.Errorf("metric not found or result is empty")
+    }
+
+	metricValue := int64(vector[0].Value)
+	metricTimestamp := vector[0].Timestamp.Time()
+	return metricValue, metricTimestamp, nil
+}
+
+func createMeteringRecords(cpuValue, memValue, storageValue int64, cpuTimestamp, memTimestamp, storageTimestamp time.Time) (marketplacemetering.BatchMeterUsageInput) {
+	timezone, _ := time.LoadLocation("UTC")
 	meteringRecords := &marketplacemetering.BatchMeterUsageInput{
-		ProductCode: aws.String(productCode), // Required
-		UsageRecords: []*marketplacemetering.UsageRecord{ // Required
-			{ // Required
-				CustomerIdentifier: aws.String(customerIdentifier), // Required
-				Dimension:          aws.String(dimensionName),     // Required
-				Quantity:           aws.Int64(value),               // Required
-				Timestamp:          aws.Time(timestamp.In(timezone)),              // Required
+		ProductCode: aws.String(productCode),
+		UsageRecords: []*marketplacemetering.UsageRecord{
+			{
+				CustomerIdentifier: aws.String(customerIdentifier),
+				Dimension:          aws.String(cpuDimension),
+				Quantity:           aws.Int64(cpuValue),
+				Timestamp:          aws.Time(cpuTimestamp.In(timezone)),
+			},
+			{
+				CustomerIdentifier: aws.String(customerIdentifier),
+				Dimension:          aws.String(memDimension),
+				Quantity:           aws.Int64(memValue),
+				Timestamp:          aws.Time(memTimestamp.In(timezone)),
+			},
+			{
+				CustomerIdentifier: aws.String(customerIdentifier),
+				Dimension:          aws.String(storageDimension),
+				Quantity:           aws.Int64(storageValue),
+				Timestamp:          aws.Time(storageTimestamp.In(timezone)),
 			},
 		},
 	}
 
-    fmt.Println(meteringRecords)
-
+	return *meteringRecords
 }
